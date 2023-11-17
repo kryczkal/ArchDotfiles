d76925357cf4f5c45179604c9fca842aa984cf74
Łukasz Kryczka <elek123456789101112@gmail.com>
2023-11-17 21:43:42 +0100

    Added river lemurs entry

---
+++ install-lemurs.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+paru -S --noconfirm lemurs # paru can be replaced by any other AUR helper
+sudo systemctl enable lemurs.service
+mkdir -p /etc/lemurs/wayland/
+echo "Lemurs will get a defualt entry for river"
+echo "Press any key to continue"
+read -n 1 -s -r
+sudo mkdir -p /etc/lemurs/wayland
+sudo cp river-lemurs-entry/river /etc/lemurs/wayland/
