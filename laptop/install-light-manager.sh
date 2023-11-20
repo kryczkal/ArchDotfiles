b36064590f7f0a9064f9e09fab6d9215f48826fe
Łukasz Kryczka <elek12345678910112@gmail.com>
2023-11-20 03:04:08 +0100

    added full laptop waybar support

--- laptop/install-light-manager.sh
+++ laptop/install-light-manager.sh
@@ -1,1 +1,4 @@
-paru -S --noconfirm light
+echo "This will install clight, usage: clight &"
+echo "Should work out of the box"
+echo "Autostart not yet configured"
+paru -S --noconfirm clight clight-gui
