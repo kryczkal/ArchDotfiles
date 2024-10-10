ff377a735d50d633a5c8d3d6174aea198f43780b
Łukasz Kryczka <elek123456789101112@gmail.com>
2024-10-10 13:48:09 +0200

    Updated nvidia installation script

--- install-additional/install-nvidia-proprietary.sh
+++ install-additional/install-nvidia-proprietary.sh
@@ -10,32 +10,76 @@ print_message "Updating system and installing NVIDIA drivers..."
 
 sudo pacman -Syu --noconfirm nvidia nvidia-utils nvidia-settings
 
-print_message "Enabling DRM Kernel Mode Setting (KMS) for NVIDIA..."
-echo "options nvidia-drm modeset=1" | sudo tee /etc/modprobe.d/nvidia-drm.conf
+print_message "Adding GBM_BACKEND and __GLX_VENDOR_LIBRARY_NAME to environment variables..."
+ENV_FILE="/etc/environment"
+if ! grep -q '^GBM_BACKEND=nvidia-drm' "$ENV_FILE"; then
+    echo "GBM_BACKEND=nvidia-drm" | sudo tee -a "$ENV_FILE"
+fi
+if ! grep -q '^__GLX_VENDOR_LIBRARY_NAME=nvidia' "$ENV_FILE"; then
+    echo "__GLX_VENDOR_LIBRARY_NAME=nvidia" | sudo tee -a "$ENV_FILE"
+fi
+if ! grep -q '^LIBVA_DRIVER_NAME=nvidia' "$ENV_FILE"; then
+    echo "LIBVA_DRIVER_NAME=nvidia" | sudo tee -a "$ENV_FILE"
+fi
+
+print_message "Removing KMS from HOOKS in initramfs and creating backup of mkinitcpio.conf"
+sudo cp /etc/mkinitcpio.conf /etc/mkinitcpio.conf.bak
+sudo sed -i '/^HOOKS=/ s/\<kms\>//g' /etc/mkinitcpio.conf
 
 print_message "Adding NVIDIA modules to initramfs..."
-sudo cp /etc/mkinitcpio.conf /etc/mkinitcpio.conf.bak
 sudo sed -i '/^MODULES=/c\MODULES=(nvidia nvidia_modeset nvidia_uvm nvidia_drm)' /etc/mkinitcpio.conf
 sudo mkinitcpio -P
 
-print_message "5. Updating bootloader with kernel parameters..."
-read -p "Is /boot/grub/grub.cfg the bootloader configuration file? (y/N):" grub_choice
+print_message "Adding a pacman hook that updates initramfs after an NVIDIA driver update..."
+HOOK_FILE="/etc/pacman.d/hooks/nvidia.hook"
+sudo mkdir -p /etc/pacman.d/hooks
+sudo tee "$HOOK_FILE" > /dev/null <<EOF
+[Trigger]
+Operation=Install
+Operation=Upgrade
+Operation=Remove
+Type=Package
+# Uncomment the installed NVIDIA package
+Target=nvidia
+#Target=nvidia-open
+#Target=nvidia-lts
+# If running a different kernel, modify below to match
+Target=linux
+
+[Action]
+Description=Updating NVIDIA module in initcpio
+Depends=mkinitcpio
+When=PostTransaction
+NeedsTargets
+Exec=/bin/sh -c 'while read -r trg; do case \$trg in linux*) exit 0; esac; done; /usr/bin/mkinitcpio -P'
+EOF
+
+print_message "Updating bootloader with kernel parameters..."
+read -p "Is /boot/grub/grub.cfg the bootloader configuration file? (y/N): " grub_choice
+
 if [[ "$grub_choice" =~ ^([yY][eE][sS]|[yY])$ ]]; then
   read -p "Is GRUB the bootloader? (y/N): " bootloader_choice
   if [[ "$bootloader_choice" =~ ^([yY][eE][sS]|[yY])$ ]]; then
-    bootloader_config_file=""
-    read -p "Add nvidia-drm.modeset=1 to the kernel parameters in the bootloader configuration file manually"
+    bootloader_config_file="/boot/grub/grub.cfg"
   else
     read -p "Enter the bootloader configuration file path: " bootloader_config_file
   fi
 else
-    bootloader_config_file="/boot/grub/grub.cfg"
+  bootloader_config_file=""
+  read -p "Add nvidia-drm.modeset=1 to the kernel parameters in the bootloader configuration file manually"
 fi
 
-if ! [[ -z "$bootloader_config_file" ]]; then
+if [[ -n "$bootloader_config_file" ]]; then
   sudo cp /etc/default/grub /etc/default/grub.bak
-  sudo sed -i 's/^GRUB_CMDLINE_LINUX_DEFAULT="\(.*\)"/GRUB_CMDLINE_LINUX_DEFAULT="\1 nvidia-drm.modeset=1"/' /etc/default/grub
-  sudo grub-mkconfig -o /boot/grub/grub.cfg
+  if ! grep -q "nvidia-drm.modeset=1" /etc/default/grub; then
+    sudo sed -i 's/^GRUB_CMDLINE_LINUX_DEFAULT="\(.*\)"/GRUB_CMDLINE_LINUX_DEFAULT="\1 nvidia-drm.modeset=1"/' /etc/default/grub
+  fi
+
+  if ! grep -q "nvidia_drm.fbdev=1" /etc/default/grub; then
+    sudo sed -i 's/^GRUB_CMDLINE_LINUX_DEFAULT="\(.*\)"/GRUB_CMDLINE_LINUX_DEFAULT="\1 nvidia_drm.fbdev=1"/' /etc/default/grub
+  fi
+
+  sudo grub-mkconfig -o "$bootloader_config_file"
 fi
 
 print_message "Installation and configuration complete."
--- install-additional/uninstall-nvidia-proprietary.sh
+++ install-additional/uninstall-nvidia-proprietary.sh
@@ -10,10 +10,18 @@ print_message "Starting NVIDIA Drivers Uninstallation"
 print_message "Removing NVIDIA drivers and utilities..."
 sudo pacman -Rns --noconfirm nvidia nvidia-utils nvidia-settings
 
-print_message "Removing NVIDIA modprobe configuration..."
-if [ -f /etc/modprobe.d/nvidia-drm.conf ]; then
-    sudo rm /etc/modprobe.d/nvidia-drm.conf
+print_message "Removing GBM_BACKEND and __GLX_VENDOR_LIBRARY_NAME from environment variables..."
+ENV_FILE="/etc/environment"
+if grep -q '^GBM_BACKEND=nvidia-drm' "$ENV_FILE"; then
+    sudo sed -i '/^GBM_BACKEND=nvidia-drm/d' "$ENV_FILE"
 fi
+if grep -q '^__GLX_VENDOR_LIBRARY_NAME=nvidia' "$ENV_FILE"; then
+    sudo sed -i '/^__GLX_VENDOR_LIBRARY_NAME=nvidia/d' "$ENV_FILE"
+fi
+if grep -q '^LIBVA_DRIVER_NAME=nvidia' "$ENV_FILE"; then
+    sudo sed -i '/^LIBVA_DRIVER_NAME=nvidia/d' "$ENV_FILE"
+fi
+
 
 print_message "Processing mkinitcpio configuration..."
 if [ -f /etc/mkinitcpio.conf.bak ]; then
@@ -23,14 +31,19 @@ if [ -f /etc/mkinitcpio.conf.bak ]; then
     else
         print_message "Not restoring backup. Removing NVIDIA modules from mkinitcpio.conf..."
         sudo sed -i 's/\<nvidia\>//g; s/\<nvidia_modeset\>//g; s/\<nvidia_uvm\>//g; s/\<nvidia_drm\>//g' /etc/mkinitcpio.conf
+        sudo sed -i '/^HOOKS=/ s/\<kms\>\|\<kms\>//g; s/^HOOKS=/HOOKS="kms /' /etc/mkinitcpio.conf
     fi
 else
     print_message "No backup of mkinitcpio.conf found. Removing NVIDIA modules from mkinitcpio.conf..."
     sudo sed -i 's/\<nvidia\>//g; s/\<nvidia_modeset\>//g; s/\<nvidia_uvm\>//g; s/\<nvidia_drm\>//g' /etc/mkinitcpio.conf
+    sudo sed -i '/^HOOKS=/ s/\<kms\>\|\<kms\>//g; s/^HOOKS=/HOOKS="kms /' /etc/mkinitcpio.conf
 fi
 
 print_message "Regenerating initramfs..."
 sudo mkinitcpio -P
+
+print_message "Deleting nvidia.hook..."
+sudo rm -f /etc/pacman.d/hooks/nvidia.hook
 
 print_message "Processing bootloader configuration..."
 if [ -f /etc/default/grub.bak ]; then
@@ -39,11 +52,11 @@ if [ -f /etc/default/grub.bak ]; then
         sudo mv /etc/default/grub.bak /etc/default/grub
     else
         print_message "Not restoring backup. Removing NVIDIA kernel parameters from /etc/default/grub..."
-        sudo sed -i 's/ nvidia-drm\.modeset=1//g' /etc/default/grub
+        sudo sed -i 's/ nvidia-drm\.modeset=1//g; s/ nvidia_drm\.fbdev=1//g' /etc/default/grub
     fi
 else
     print_message "No backup of /etc/default/grub found. Removing NVIDIA kernel parameters from /etc/default/grub..."
-    sudo sed -i 's/ nvidia-drm\.modeset=1//g' /etc/default/grub
+    sudo sed -i 's/ nvidia-drm\.modeset=1//g; s/ nvidia_drm\.fbdev=1//g' /etc/default/grub
 fi
 
 sudo grub-mkconfig -o /boot/grub/grub.cfg
