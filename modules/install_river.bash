08a929af076df1e73b41b56577cfda42cc3e35e4
Łukasz Kryczka <lukasz.kryczka000@gmail.com>
2025-07-25 00:33:39 +0200

    River module fix

--- modules/install_river.bash
+++ modules/install_river.bash
@@ -6,8 +6,8 @@ IFS=$'\n\t'
 
 source "$(dirname "$0")/../lib/utils.bash"
 
-print_message "Installing River, Wayland, wlroots, and wayland-protocols..."
-sudo pacman -S --noconfirm river wayland wlroots wayland-protocols
+print_message "Installing River, Wayland, and wayland-protocols..."
+sudo pacman -S --noconfirm river wayland wayland-protocols
 
 print_message "Appending XDG_SESSION_TYPE=wayland to ~/.profile..."
 append_line_to_file_if_not_exists "XDG_SESSION_TYPE=wayland" "$HOME/.profile"
